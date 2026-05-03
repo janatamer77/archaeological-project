1c001c74c1b405ded07e14f3f589640e68bc6284
sshaabanismail-design <s.shaaban.ismail@gmail.com>
2026-05-04 01:05:54 +0300

    Refactor theme and search functionality in script.js

--- script.js
+++ script.js
@@ -31,3 +31,22 @@ themeBtn.addEventListener('click', () => {
      }
    });
  });
+let pho=document.querySelectorAll('.gallery img');
+let lightbox= document.getElementById('lightbox');
+let lightboxImg= document.querySelector('.lightbox-img');
+let closeImg= document.querySelector('.close-img');
+
+pho.forEach(img => {
+    img.addEventListener('click',function(){
+        lightboxImg.src = this.src;
+        lightbox.style.display = "flex";
+    });
+});
+closeImg.addEventListener('click',function(){
+    lightbox.style.display = "none";
+});
+lightbox.addEventListener('click',function(e){
+    if (e.target !== lightboxImg){
+        lightbox.style.display = "none";
+    }
+});
