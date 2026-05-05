ae3687dcbf0e4ceb3bbbdc5ded5814b0421dd940
janatamer77 <janatamer053@gmail.com>
2026-05-05 15:35:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//  theme 
+
 const themeBtn = document.getElementById('theme-b');
 const bodyElement = document.body;
 if (localStorage.getItem('theme') === 'dark') {
@@ -12,27 +12,26 @@ themeBtn.addEventListener('click', () => {
         localStorage.setItem('theme', 'light');
     }
 });
-// // search
-// let b = document.querySelector(".search-b");
-//   let s = document.getElementById("search");
-// b.addEventListener("click", function () {
-//   s.classList.toggle("active");
-//       });
-//       let input = document.querySelector("#search input");
-//       let cards = document.querySelectorAll(".card");
-//       input.addEventListener("keyup", function () {
-//       let value = input.value.toLowerCase();
-//       cards.forEach(card => {
-//      let name = card.querySelector(".name").textContent.toLowerCase();
-//     if (name.includes(value)) {
-//     card.classList.remove("hidden");
-//     } else {
-//     card.classList.add("hidden");
-//     }
-//    });
-// });
 
- 
+let b = document.querySelector(".search-b");
+  let s = document.getElementById("search");
+b.addEventListener("click", function () {
+  s.classList.toggle("active");
+      });
+      let input = document.querySelector("#search input");
+      let cards = document.querySelectorAll(".card");
+      input.addEventListener("keyup", function () {
+      let value = input.value.toLowerCase();
+      cards.forEach(card => {
+     let name = card.querySelector(".name").textContent.toLowerCase();
+    if (name.includes(value)) {
+    card.classList.remove("hidden");
+    } else {
+    card.classList.add("hidden");
+    }
+   });
+});
+
 const layoutBtn = document.querySelector(".layout");
 const rowS = document.querySelector(".row-s");
 let isReversed = localStorage.getItem("layout") === "reversed";
@@ -75,70 +74,55 @@ lightbox.addEventListener('click',function(e){
     }
 });
 
-// Sign up & Login Validation
 
 function signup() {
 let email = document.getElementById('email').value;
 let password = document.getElementById('password').value;
 let password2 = document.getElementById('password2').value;
-
 let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-
 if (!email.match(emailPattern)) {
 error.innerText = "Please enter a valid email address!";
 alert('Please enter a valid email address!');
 return false;
 }
-
 if (email == '' || password == '') {
 alert('All fields are required!');
 return;
 }
-
 if (password != password2) {
 alert('Password does not match');
 return;
 }
-
 if (password.length < 6) {
 alert('Password must be at least 6 characters!');
 return;
 }
-
 localStorage.setItem('email', email);
 localStorage.setItem('password', password);
 alert('Succeful Register');
 window.location.href = "login.html";
 return false;
 }
-
 function login() {
 let email = document.getElementById('email').value;
 let password = document.getElementById('password').value;
 let error = document.getElementById("error");
-
-
 let savedEmail = localStorage.getItem('email');
 let savedPassword = localStorage.getItem('password');
-
 let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-
 if (email === "" || password === "") {
 error.innerText = "All fields are required!";
 return false;
 }
-
 if (!email.match(emailPattern)) {
 error.innerText = "Please enter a valid email address!";
 alert('Please enter a valid email address!');
 return false;
 }
-
 if (password.length < 6) {
 error.innerText = "Password must be at least 6 characters!";
 return false;
 }
-
 if (email == savedEmail && password == savedPassword) {
 sessionStorage.setItem('isLoggedIn', 'true');
 alert('Welcome to Secret of Kemet');
