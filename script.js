5290faf6bdd17ccd546116523d952e67f2115060
janatamer77 <janatamer053@gmail.com>
2026-05-05 21:18:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
 const themeBtn = document.getElementById('theme-b');
 const bodyElement = document.body;
 if (localStorage.getItem('theme') === 'dark') {
     bodyElement.classList.add('dark-mode');
 }
-
 themeBtn.addEventListener('click', () => {
     bodyElement.classList.toggle('dark-mode');
     if (bodyElement.classList.contains('dark-mode')) {
