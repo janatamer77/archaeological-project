92a6009eca97ad53d8472377b4676a5d45d0b7fb
no7131642-jpg <no7131642@gmail.com>
2026-05-05 21:01:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const bodyElement = document.body;
 if (localStorage.getItem('theme') === 'dark') {
     bodyElement.classList.add('dark-mode');
 }
+
 themeBtn.addEventListener('click', () => {
     bodyElement.classList.toggle('dark-mode');
     if (bodyElement.classList.contains('dark-mode')) {
