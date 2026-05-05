7c7f689f0e9f145a64163a89203310da7665f37b
janatamer77 <janatamer053@gmail.com>
2026-05-05 15:05:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -33,26 +33,26 @@ b.addEventListener("click", function () {
 });
 
 
-const layoutBtn = document.querySelector(".layout");
-const rowS = document.querySelector(".row-s");
-let rows = document.querySelectorAll(".row");
-let isReversed = localStorage.getItem("layout") === "reversed";
-if (isReversed) {
-  rows = document.querySelectorAll(".row");
-  rowS.insertBefore(rows[1], rows[0]);
-}
-layoutBtn.onclick = function () {
-  rows = document.querySelectorAll(".row");
-  if (!isReversed) {
-    rowS.insertBefore(rows[1], rows[0]);
-    localStorage.setItem("layout", "reversed");
-    isReversed = true;
-  } else {
-    rowS.insertBefore(rows[0], rows[1]);
-    localStorage.setItem("layout", "normal");
-    isReversed = false;
-  }
-};
+// const layoutBtn = document.querySelector(".layout");
+// const rowS = document.querySelector(".row-s");
+// let rows = document.querySelectorAll(".row");
+// let isReversed = localStorage.getItem("layout") === "reversed";
+// if (isReversed) {
+//   rows = document.querySelectorAll(".row");
+//   rowS.insertBefore(rows[1], rows[0]);
+// }
+// layoutBtn.onclick = function () {
+//   rows = document.querySelectorAll(".row");
+//   if (!isReversed) {
+//     rowS.insertBefore(rows[1], rows[0]);
+//     localStorage.setItem("layout", "reversed");
+//     isReversed = true;
+//   } else {
+//     rowS.insertBefore(rows[0], rows[1]);
+//     localStorage.setItem("layout", "normal");
+//     isReversed = false;
+//   }
+// };
 
 
 
