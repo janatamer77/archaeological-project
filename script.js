a8210f08d12949c79af6806ba8aff6f8df0a6d82
janatamer77 <janatamer053@gmail.com>
2026-05-05 10:44:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,11 +24,6 @@ b.addEventListener("click", function () {
       let value = input.value.toLowerCase();
       cards.forEach(card => {
      let name = card.querySelector(".name").textContent.toLowerCase();
-     //  if (name.includes(value)) {
-     //  card.style.display = "block";
-     // } else {
-     //  card.style.display = "none";
-     // }
     if (name.includes(value)) {
     card.classList.remove("hidden");
     } else {
